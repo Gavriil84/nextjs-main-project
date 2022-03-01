f6d748a5029cb7e5984da4dfbe70826b0ef9db77
Gavriil84 <gabwt.84@gmail.com>
2022-02-28 22:52:52 -0800

    add: set component and props for meetup details

---
+++ pages/[meetupid]/index.js
@@ -0,0 +1,14 @@
+
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+function MeetUpDetails() {
+    return (
+        <MeetupDetail
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Paris_July_2011-30.jpg/636px-Paris_July_2011-30.jpg"
+            title="A First Meetup"
+            address="Some Street 5, Some City"
+            description="The meetup description" />
+    )
+}
+
+export default MeetUpDetails
